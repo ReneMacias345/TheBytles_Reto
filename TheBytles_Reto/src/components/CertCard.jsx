f373a42e6c5bee235008de3eb0448c9afa358854
Pedro Palafox <A01027841@tec.mx>
2025-05-08 20:02:27 -0600

    Certificaciones en perfil

--- TheBytles_Reto/src/components/CertCard.jsx
+++ TheBytles_Reto/src/components/CertCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const CertCard = ({ certName, description, date, expiration }) => {
   return (
-    <div className="bg-white p-6 rounded-3xl shadow-md flex flex-col justify-between">
+    <div className="bg-gray-100 p-6 rounded-3xl shadow-md flex flex-col justify-between">
       <div>
         <h3 className="text-lg font-bold text-gray-800 mb-2">{certName}</h3>
         <p className="text-sm text-gray-600 mb-4">{description}</p>
