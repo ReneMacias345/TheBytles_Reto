aac9fd4af4e8115d6b08e2e79a3eb29d5fecd427
AnaKx <A07192068@tec.mx>
2025-05-11 18:50:05 -0600

    Http -> https del host de back

--- TheBytles_Reto/src/pages/Perfil.jsx
+++ TheBytles_Reto/src/pages/Perfil.jsx
@@ -413,7 +413,7 @@ export const Perfil = () => {
     // ✅ Actualizar estado local en el frontend
     setUserData(prev => ({ ...prev, cv_url: publicUrl }));
     alert("CV uploaded and linked successfully!");
-    await fetch("http://thebytlesbackend-production.up.railway.app/generate-summary", {
+    await fetch("https://thebytlesbackend-production.up.railway.app/generate-summary", {
       method: "POST",
       body: JSON.stringify({ user_id: userId }),
       headers: { "Content-Type": "application/json" },
