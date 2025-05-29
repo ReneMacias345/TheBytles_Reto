6312711bbe2d09cb05233fc58adf0641087aac12
Pedro Palafox <A01027841@tec.mx>
2025-05-29 16:52:59 -0600

    Bugs de Recomendaciones

--- TheBytles_Reto/src/components/RecCert.jsx
+++ TheBytles_Reto/src/components/RecCert.jsx
@@ -1,24 +1,28 @@
 export const RecCert = ({ title, description, image, link ,capability}) => {
   return (
-    <div className="w-[300px] h-[340px] bg-white rounded-2xl shadow-lg transition-transform transform hover:-translate-y-2 hover:shadow-2xl flex flex-col flex-shrink-0">
+    <div className="w-[300px] h-[350px] bg-white rounded-2xl shadow-lg transition-transform transform hover:-translate-y-2 hover:shadow-2xl flex flex-col flex-shrink-0">
 
       <img
         src={image}
         alt={title}
-        className="w-full h-[170px] object-contain rounded-t-2xl p-1"
+        className="w-full h-[130px] object-contain rounded-t-2xl p-2"
       />
       
 
       <div className="p-4 flex flex-col h-full">
-        <h3 className="text-md font-semibold text-[#A100FF] mb-2">{title}</h3>
+        <h3 className="text-md font-semibold text-[#A100FF] mb-2 break-words whitespace-normal leading-snug">
+            {title}</h3>
         
 
         <div className="flex-grow overflow-hidden">
-          <p className="text-xs text-gray-600 break-words whitespace-normal text-justify">
+          <p className="text-xs text-black break-words whitespace-normal text-justify">
             {description}
           </p>
         </div>
-        <h3> {capability}</h3>
+
+        <h4 className="text-xs font-semibold text-[#A100FF] break-words whitespace-normal leading-snug">
+             Capability:{capability}
+             </h4>
         
         <a
           href={link}
