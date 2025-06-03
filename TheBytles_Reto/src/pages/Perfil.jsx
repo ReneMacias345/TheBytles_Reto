7c6c62d64508e90ff777583f1359aa54aecd5c49
Pedro Palafox <A01027841@tec.mx>
2025-06-03 00:37:01 -0600

    Cambiar nombre bio

--- TheBytles_Reto/src/pages/Perfil.jsx
+++ TheBytles_Reto/src/pages/Perfil.jsx
@@ -872,13 +872,29 @@ const navigate = useNavigate();
         </p>      
         <div className="mt-4">
           <button
-            name = "Bio"
+            name="Bio"
             onClick={handleOpenBioForm}
             className="border border-gray-300 px-4 py-2 rounded-full text-white bg-[#A100FF] hover:bg-[#A100FF] transition-colors"
           >
-            Bio
+            <div className="flex items-center gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-5 h-5 mr-1"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                />
+              </svg>
+              About Me 
+            </div>
           </button>
-        </div>
+          </div>
         <input
           type="file"
           ref={profilePicInputRef}
